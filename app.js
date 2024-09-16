2719653072e3b4a279cf6744298427d6d4a2d13d
Samreen Fatima <irfankhans786787789@gmail.com>
2024-09-16 20:52:23 +0500

    completed

---
+++ app.js
@@ -0,0 +1,41 @@
+// var food = prompt("Enter your food");
+// if (food === "baryani") {
+//     alert("sahi he ajao")
+// }
+// else if (food ==="buryer"){
+//     alert("sahi he 2 leke ajao")
+// }
+// else if (food === "fries"){
+//     alert("50 k paker lao")
+// }
+// else {
+//     alert("ghr mat ao")
+// }
+
+
+// var food = prompt("Enter your food")
+
+// if (food === "baryani"){
+//     var extraItem = prompt("Enter yor extraItem") 
+//         if ( extraItem === "raita"){
+//            alert("sahi he leke agao")
+//         }  
+//         else{
+//             alert("chordo bas " + food + " leke ajo ")
+//         }
+// }
+// var lang1 = prompt("Enter yor lang 1")
+// var lang2 = prompt("Enter yor lang 2")
+// if ( lang1 === "html" || lang2 == "css") {
+//     alert("yor are hired!")
+// }
+// else{
+//     alert("not hired!")
+// }
+var num = 400;
+var num2 = 500
+var totle = 900;
+if ( totle < num + num2){
+    console.log("chal jya");
+   
+}
